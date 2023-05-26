2038a8f76f97a57313d89e55d5c445106176a4c9
Guilherme Machado <guifermach@hotmail.com>
2023-05-26 10:34:41 -0300

    Adequando StatusCode de cada endpoint

--- Program.cs
+++ Program.cs
@@ -8,18 +8,26 @@ var app = builder.Build();
 app.MapPost("/products", (Produto produto) =>
 {
     ProductRepository.Add(produto);
+    return Results.Created($"/products/{produto.Code}", produto.Code);
 });
 
 app.MapGet("/products/{code}",([FromRoute] string code) =>
 {
     var product = ProductRepository.GetBy(code);
-    return product;
+    if(product != null)
+    {
+        return Results.Ok(product);    
+    }else
+    {
+        return Results.NotFound();
+    }
 });
 
 app.MapPut("/products",(Produto produto) =>
 {
     var productSaved = ProductRepository.GetBy(produto.Code);
     productSaved.Name = produto.Name;
+    return Results.Ok();
 
 });
 
@@ -28,23 +36,27 @@ app.MapDelete("/products/{code}",([FromRoute] string code) =>
 {
     var productSaved = ProductRepository.GetBy(code);
     ProductRepository.Remove(productSaved);
-
+    return Results.Ok();
 });
 
+
+app.MapGet("/configuration/database", (IConfiguration configuration)=>
+{
+    return Results.Ok($"{configuration["database:Connection"]}/{configuration["database:Port"]}");
+});
 
 app.Run();
 
 public static class ProductRepository
 {
-    public static List<Produto> Products { get; set; }
+    public static List<Produto> Products { get; set; } =  Products = new List<Produto>();
+
 
     public static void Add(Produto product)
     {
-        if(Products == null)
-        {
-            Products = new List<Produto>();
-        }
+
         Products.Add(product);
+        
     }
 
     public static Produto GetBy(string code )
